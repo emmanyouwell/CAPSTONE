21d2886981e437f02d6415f4b3fe3363cc06be5a
emmanyouwell <emingala02@gmail.com>
2024-10-09 21:55:20 +0800

    added padding

--- frontend/styles/Styles.js
+++ frontend/styles/Styles.js
@@ -89,7 +89,8 @@ export const SuperAdmin = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     marginVertical: 20,
-    textAlign: 'center'
+    textAlign: 'center',
+    padding: 10,
   },
   menuContainer: {
     flex: 1,
