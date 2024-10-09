c6664e0fe4400a633dac8e8e06440969d85379ee
emmanyouwell <emingala02@gmail.com>
2024-10-09 21:32:26 +0800

    returned to normal

--- frontend/components/Home.jsx
+++ frontend/components/Home.jsx
@@ -4,7 +4,7 @@ import { defaultStyle, buttonStyle, loginStyle } from '../styles/Styles'
 const Home = ({navigation}) => {
   return (
     <View style={defaultStyle.container}>
-        <Text>Development Home</Text>
+        <Text>Home</Text>
         <TouchableOpacity onPress={()=>navigation.navigate('login')} style={buttonStyle.defaultBtn}><Text style={buttonStyle.btnText}>Login</Text></TouchableOpacity>
         <TouchableOpacity onPress={()=>navigation.navigate('superadmin_dashboard')} style={buttonStyle.defaultBtn}><Text style={buttonStyle.btnText}>Dashboard</Text></TouchableOpacity>
     </View>
