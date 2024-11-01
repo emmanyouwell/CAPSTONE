35072a4b91b415f1d956225b63b629528dff3ba9
reden21345 <mirandilla395@gmail.com>
2024-11-01 12:33:15 +0800

    comment filter

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -17,21 +17,21 @@ class APIFeatures {
         return this;
     }
 
-    filter() {
+    // filter() {
 
-        const queryCopy = { ...this.queryStr };
+    //     const queryCopy = { ...this.queryStr };
 
-        // Removing fields from the query
-        const removeFields = ['keyword', 'limit', 'page']
-        removeFields.forEach(el => delete queryCopy[el]);
+    //     // Removing fields from the query
+    //     const removeFields = ['keyword', 'limit', 'page']
+    //     removeFields.forEach(el => delete queryCopy[el]);
 
-        // // Advance filter for price, ratings etc
-        let queryStr = JSON.stringify(queryCopy);
-        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`)
+    //     // // Advance filter 
+    //     let queryStr = JSON.stringify(queryCopy);
+    //     queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`)
 
-        this.query = this.query.find(JSON.parse(queryStr));
-        return this;
-    }
+    //     this.query = this.query.find(JSON.parse(queryStr));
+    //     return this;
+    // }
 
     pagination(resPerPage) {
         const currentPage = Number(this.queryStr.page) || 1;
