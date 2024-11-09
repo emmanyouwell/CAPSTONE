044333dee69cdc01a75d93aaf0abaa9baa7d88cc
reden21345 <mirandilla395@gmail.com>
2024-11-09 13:01:04 +0800

    Donors CRUD

---
+++ backend/routes/donor.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+const { 
+    allDonors,
+    createDonor,
+    getDonorDetails,
+    updateDonor,
+    deleteDonor,
+} = require('../controllers/donorController');
+
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
+
+// Super Admin Routes
+router.route('/donors')
+    .get(isAuthenticatedUser, authorizeRoles('SuperAdmin', 'Admin'), allDonors)
+    .post(isAuthenticatedUser, authorizeRoles('SuperAdmin', 'Admin'), createDonor);
+router.route('/donor/:id')
+    .get(isAuthenticatedUser, authorizeRoles('SuperAdmin', 'Admin'), getDonorDetails)
+    .put(isAuthenticatedUser, authorizeRoles('SuperAdmin', 'Admin'), updateDonor)
+    .delete(isAuthenticatedUser, authorizeRoles('SuperAdmin', 'Admin'), deleteDonor);
+
+
+module.exports = router;
