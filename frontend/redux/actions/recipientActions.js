d82b17a7ca200e0c7c43552792529ef96d66b1a9
emmanyouwell <emingala02@gmail.com>
2024-11-22 06:50:25 +0800

    recipient fetch and search

---
+++ frontend/redux/actions/recipientActions.js
@@ -0,0 +1,87 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { authenticate, getToken, logout } from '../../utils/helper';
+import { REACT_APP_API_URL } from '@env';
+
+export const getRecipients = createAsyncThunk(
+    'recipient/getRecipients',
+    async (query, thunkAPI) => {
+        
+        const token = await getToken();
+        console.log('Token Retrieved:', token);
+
+        if (!token) {
+            throw new Error('No token available');
+        }
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`
+            },
+            withCredentials: true
+        }
+        try {
+            let urlString = ''
+            if (query){
+                urlString = `${REACT_APP_API_URL}/api/v1/patients?search=${query}`
+            }
+            else {
+                urlString = `${REACT_APP_API_URL}/api/v1/patients`
+            }
+            const response = await axios.get(urlString, config);
+
+            return response.data;
+
+        } catch (error) {
+            
+            return thunkAPI.rejectWithValue(error.message);
+        }
+    }
+)
+
+export const logoutUser = createAsyncThunk(
+    'user/logoutUser',
+    async (_, thunkAPI) => {
+        try {
+            const response = await axios.get(
+                `${REACT_APP_API_URL}/api/v1/logout`,
+                {},
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    withCredentials: true, // Assuming the API uses cookies or session for auth
+                }
+            );
+            logout(() => { });
+            return response.data; // Optionally return the response if necessary for further actions
+
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error.message);
+        }
+    }
+);
+
+export const getUserDetails = createAsyncThunk(
+    'user/getUserDetails',
+    async (_, thunkAPI) => {
+        try {
+            const response = await axios.get(
+                `${REACT_APP_API_URL}/api/v1/me`,
+                {},
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    withCredentials: true, // Assuming the API uses cookies or session for auth
+                }
+            );
+
+            return response.data; // Optionally return the response if necessary for further actions
+
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error.message);
+        }
+    }
+);
---
+++ frontend/redux/slices/recipientSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { getRecipients } from '../actions/recipientActions';
+export const recipientSlice = createSlice({
+  name: 'donor',
+  initialState: {
+    recipients: [],
+    loading: false, // Useful for async actions like login/signup
+    error: null, // To handle errors
+    count: 0,
+    pageSize: 0,
+  },
+  reducers: {
+  },
+  extraReducers: (builder) => {
+    builder
+      
+      .addCase(getRecipients.pending, (state, action) => {
+        state.loading = true;
+      })
+      .addCase(getRecipients.fulfilled, (state, action) => {
+        state.loading = false;
+        state.recipients = action.payload.patients;
+        state.count = action.payload.count;
+        state.pageSize = action.payload.pageSize;
+      })
+      .addCase(getRecipients.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload; // The error message passed in rejectWithValue
+      })
+      
+  },
+});
+
+export default recipientSlice.reducer;
