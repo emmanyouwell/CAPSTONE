8af98ada32d0d770a694ccdb5019f8bf8fe25b8a
emmanyouwell <emingala02@gmail.com>
2025-01-22 13:34:03 +0800

    updated menugrid for admin

--- frontend/components/Superadmin/MenuGrid.jsx
+++ frontend/components/Superadmin/MenuGrid.jsx
@@ -20,10 +20,10 @@ const adminItems = [
   { id: '1', title: 'Donor Records', route: 'superadmin_donor_record', image: require('../../assets/image/donor-records.png') },
   { id: '2', title: 'Recipient Records', route: 'superadmin_recipient_record', image: require('../../assets/image/recipient-records.png') },
   { id: '3', title: 'Inventory', route: 'superadmin_inventories', image: require('../../assets/image/inventory-and-metrics.png') },
-  { id: '4', title: 'Requesting Milk', route: 'MilkRequest', image: require('../../assets/image/resources-management.png') },
+  { id: '4', title: 'Resources Management', route: 'superadmin_articles', image: require('../../assets/image/resources-management.png') },
   { id: '5', title: 'Schedules', route: 'superadmin_schedules', image: require('../../assets/image/calendar.png') },
   { id: '6', title: 'Announcement Creation', image: require('../../assets/image/announcement-creation.png') },
-  
+  { id: '7', title: 'Milk Requesting', route: 'MilkRequest', image: require('../../assets/image/resources-management.png') },
 ];
 
 const staffItems = [
