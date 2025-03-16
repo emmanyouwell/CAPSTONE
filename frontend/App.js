b61b4486bf4b9254accd9d0a81f9b66845270ef7
emmanyouwell <emingala02@gmail.com>
2025-03-16 14:23:54 +0800

    added paper provider

--- frontend/App.js
+++ frontend/App.js
@@ -1,21 +1,23 @@
 import Main from './Main';
-import {Provider} from 'react-redux';
-import {store} from './redux/store';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 import { NotificationProvider } from "./context/NotificationContext";
 import * as Notifications from 'expo-notifications';
-
+import { Provider as PaperProvider } from 'react-native-paper';
 export default function App() {
   Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: true,
-    shouldSetBadge: false,
-  }),
-});
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+      shouldSetBadge: false,
+    }),
+  });
   return (
     <NotificationProvider>
       <Provider store={store}>
-        <Main/>
+        <PaperProvider>
+          <Main />
+        </PaperProvider>
       </Provider>
     </NotificationProvider>
   );
