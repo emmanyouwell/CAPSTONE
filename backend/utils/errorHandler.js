15b7f5492588957fd95052c29e3222f26fccb285
emmanyouwell <emingala02@gmail.com>
2025-03-18 02:05:51 +0800

    updated error handler

--- backend/utils/errorHandler.js
+++ backend/utils/errorHandler.js
@@ -1,10 +1,14 @@
 class ErrorHandler extends Error {
-    constructor(message, statusCode){
+    constructor(message, statusCode) {
         super(message);
         this.statusCode = statusCode;
 
-        // Error.captureStackTrace(this, this.constructor)
+        Error.captureStackTrace(this, this.constructor)
     }
+    toJSON() {
+        return { message: this.message, statusCode: this.statusCode };
+    }
+
 }
 
 module.exports = ErrorHandler;
