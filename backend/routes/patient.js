355ad691925a0316176c458e64537eaa91bb5a4f
reden21345 <mirandilla395@gmail.com>
2025-03-18 18:13:17 +0800

    updated patient backend (CRUD)

--- backend/routes/patient.js
+++ backend/routes/patient.js
@@ -16,7 +16,7 @@ router.route('/patients')
     .get(isAuthenticatedUser, authorizeRoles('Staff', 'SuperAdmin', 'Admin'), allPatients)
     .post(isAuthenticatedUser, authorizeRoles('Staff','SuperAdmin', 'Admin'), createPatient);
 router.route('/patient/:id')
-    .get(isAuthenticatedUser, authorizeRoles('SuperAdmin', 'Admin'), getPatientDetails)
+    .get(isAuthenticatedUser, authorizeRoles('SuperAdmin', 'Admin', 'Staff'), getPatientDetails)
     .put(isAuthenticatedUser, authorizeRoles('Staff', 'SuperAdmin', 'Admin'), updatePatient)
     .delete(isAuthenticatedUser, authorizeRoles('SuperAdmin', 'Admin'), deletePatient);
 
