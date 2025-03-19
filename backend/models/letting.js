5b8d72ffec6658c7eb21297b852eb7dcc605dbe0
reden21345 <mirandilla395@gmail.com>
2025-03-19 08:47:39 +0800

    remove description in model and actDetails in controller

--- backend/models/letting.js
+++ backend/models/letting.js
@@ -11,9 +11,6 @@ const lettingSchema = mongoose.Schema({
         type: String,
         required: true
     },
-    description: {
-        type: String
-    },
     actDetails: {
         start: {
             type: Date,
