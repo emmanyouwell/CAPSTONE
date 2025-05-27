719f95433ef687fe737226a0e43369afe6e0151e
emmanyouwell <emingala02@gmail.com>
2025-05-27 16:05:49 +0800

    automatically add age from birthday

---
+++ backend/utils/helper.js
@@ -0,0 +1,16 @@
+export const calculateAge = (birthday) => {
+    const birthDate = new Date(birthday);
+    const today = new Date();
+
+    let age = today.getFullYear() - birthDate.getFullYear();
+
+    const hasHadBirthdayThisYear =
+        today.getMonth() > birthDate.getMonth() ||
+        (today.getMonth() === birthDate.getMonth() && today.getDate() >= birthDate.getDate());
+
+    if (!hasHadBirthdayThisYear) {
+        age--;
+    }
+
+    return age;
+}
